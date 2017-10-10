4c1c118b0498271fbc8b1fc354897c97204cce56
Matheus Bigarelli <matheusbigarelli@uol.com.br>
2017-10-09 23:24:28 -0300

    fim

--- src/main/java/utfpr/dainf/ct/ed/exemplo/ArvoreBinaria.java
+++ src/main/java/utfpr/dainf/ct/ed/exemplo/ArvoreBinaria.java
@@ -1,14 +1,15 @@
 package utfpr.dainf.ct.ed.exemplo;
 
+import java.util.LinkedList;
 import java.util.Stack;
 
 /**
- * UTFPR - Universidade Tecnológica Federal do Paraná
- * DAINF - Departamento Acadêmico de Informática
+ * UTFPR - Universidade TecnolÃ³gica Federal do ParanÃ¡
+ * DAINF - Departamento AcadÃªmico de InformÃ¡tica
  * 
- * Exemplo de implementação de árvore binária.
+ * Exemplo de implementaÃ§Ã£o de Ã¡rvore binÃ¡ria.
  * @author Wilson Horstmeyer Bogado <wilson@utfpr.edu.br>
- * @param <E> O tipo do valor armazenado nos nós na árvore
+ * @param <E> O tipo do valor armazenado nos nÃ³s na Ã¡rvore
  */
 public class ArvoreBinaria<E> {
     
@@ -19,26 +20,42 @@ public class ArvoreBinaria<E> {
     // para percurso iterativo
     private boolean inicio = true;
     private Stack<ArvoreBinaria<E>> pilha;
+    private LinkedList<ArvoreBinaria<E>> fila;
     private ArvoreBinaria<E> ultimoVisitado;
+    private boolean visitado = false;
+
+    public static void main(String[] args)
+    {
+        ArvoreBinaria<Integer> a = new ArvoreBinaria(5);
+        a.insereEsquerda(6);
+        a.insereEsquerda(8);
+        a.insereEsquerda(2);
+        a.insereEsquerda(3);
+        a.insereEsquerda(4);
+        a.insereEsquerda(10);
+        
+        a.visitaEmOrdem();
+        //a.proximoPreOrdem();
+    }
 
     /**
-     * Cria uma árvore binária com dado nulo na raiz.
+     * Cria uma Ã¡rvore binÃ¡ria com dado nulo na raiz.
      */
     public ArvoreBinaria() {
     }
 
     /**
-     * Cria uma árvore binária com dado {@code dado} na raiz.
-     * @param valor O dado do nó raiz
+     * Cria uma Ã¡rvore binÃ¡ria com dado {@code dado} na raiz.
+     * @param valor O dado do nÃ³ raiz
      */
     public ArvoreBinaria(E dado) {
         this.dado = dado;
     }
     
     /**
-     * Adiciona um nó à esquerda do nó corrente.
-     * @param dado O dado associado ao nó inserido.
-     * @return A árvore adicionada ao nó
+     * Adiciona um nÃ³ Ã  esquerda do nÃ³ corrente.
+     * @param dado O dado associado ao nÃ³ inserido.
+     * @return A Ã¡rvore adicionada ao nÃ³
      */
     public ArvoreBinaria<E> insereEsquerda(E dado) {
         ArvoreBinaria<E> e = esquerda;
@@ -48,9 +65,9 @@ public class ArvoreBinaria<E> {
     }
     
     /**
-     * Adiciona um nó à esquerda do nó corrente.
-     * @param dado O dado associado ao nó inserido.
-     * @return A árvore adicionada ao nó
+     * Adiciona um nÃ³ Ã  esquerda do nÃ³ corrente.
+     * @param dado O dado associado ao nÃ³ inserido.
+     * @return A Ã¡rvore adicionada ao nÃ³
      */
     public ArvoreBinaria<E> insereDireita(E dado) {
         ArvoreBinaria<E> d = direita;
@@ -60,19 +77,19 @@ public class ArvoreBinaria<E> {
     }
     
     /**
-     * Implementação padrão que exibe o dado armazenado no nó usando
-     * o método {@code toString() }.
+     * ImplementaÃ§Ã£o padrÃ£o que exibe o dado armazenado no nÃ³ usando
+     * o mÃ©todo {@code toString() }.
      * Pode ser sobrecarregado em classes derivadas para implementar outras
      * formas de visita.
-     * @param no O nó a ser visitado
+     * @param no O nÃ³ a ser visitado
      */
     protected void visita(ArvoreBinaria<E> no) {
         System.out.print(" " + no.dado);
     }
     
     /**
-     * Visita os nós da subárvore em-ordem.
-     * @param raiz A raiz da subárvore
+     * Visita os nÃ³s da subÃ¡rvore em-ordem.
+     * @param raiz A raiz da subÃ¡rvore
      */
     public void visitaEmOrdem(ArvoreBinaria<E> raiz) {
         if (raiz != null) {
@@ -83,11 +100,169 @@ public class ArvoreBinaria<E> {
     }
     
     /**
-     * Visita os nós da árvore em-ordem a partir da raiz.
+     * Visita os nÃ³s da Ã¡rvore em-ordem a partir da raiz.
      */
     public void visitaEmOrdem() {
         visitaEmOrdem(this);
     }
+
+
+//----------------------------------------------------------------------------------------------------------------------------------
+
+    // Visita pre ordem recursivo
+    public void visitaPreOrdem()
+    {
+        visitaPreOrdem(this);
+    }
+
+    public void visitaPreOrdem(ArvoreBinaria<E> a)
+    {
+        visita(a);
+        if (a != null)
+        {
+            if (a.esquerda != null)
+                visitaPreOrdem(a.esquerda);
+
+            if (a.direita != null)
+                visitaPreOrdem(a.direita);
+        }
+    }
+
+
+    // Visita pre ordem iterativo
+    public ArvoreBinaria<E> proximoPreOrdem()
+    {
+        ArvoreBinaria<E> resultado;
+        resultado = null;
+     
+        if (inicio)
+        {
+            reinicia();
+            pilha.push(this);
+            inicio = false;
+        }
+        
+        if (!pilha.isEmpty())
+        {
+            resultado = pilha.pop();
+            
+            if (resultado.direita != null)
+                pilha.push(resultado.direita);
+            if (resultado.esquerda != null)
+                pilha.push(resultado.esquerda);
+        }
+
+        if (resultado == null)
+        {
+            inicio = true;
+            tiraVisitado(this);
+        }
+
+        return resultado;
+    }
+
+//----------------------------------------------------------------------------------------------------------------------------------
+    // Visita pos ordem recursivo
+    public void visitaPosOrdem() { visitaPosOrdem(this); }
+
+    public void visitaPosOrdem(ArvoreBinaria<E> no)
+    {
+        if (no.esquerda != null)
+            visitaPosOrdem(no.esquerda);
+        if (no.direita != null)
+            visitaPosOrdem(no.direita);
+        visita(no);
+    }
+
+    // Visita pos ordem iterativo
+    public ArvoreBinaria<E> proximoPosOrdem()
+    {
+        ArvoreBinaria<E> resultado, pai = null;
+        resultado = this;
+
+        while (resultado != null)
+        {
+            if (resultado.visitado == false)
+            {
+                pai = resultado;
+                resultado = resultado.esquerda;
+                if (resultado == null && pai != null)
+                    resultado = pai.direita;
+            }
+            else
+            {
+                if (pai != null)
+                   resultado = pai.direita;   
+                if (resultado != null && resultado.visitado == true)
+                    break;
+            }
+        }
+
+        resultado = pai;
+        
+        if (resultado == null)
+        {
+            inicio = true;
+            tiraVisitado(this);
+        }
+
+        else
+            resultado.visitado = true;
+
+        return resultado;
+    }
+
+
+    private void tiraVisitado(ArvoreBinaria<E> a)
+    {
+        if (a != null)
+        {
+            a.visitado = false;
+            tiraVisitado(a.esquerda);
+            tiraVisitado(a.direita);
+        }
+    }
+
+
+//----------------------------------------------------------------------------------------------------------------------------------
+
+    //Visita em nível iterativo
+    public ArvoreBinaria<E> proximoEmNivel()
+    {
+        ArvoreBinaria<E> resultado;
+        if (fila == null)
+            fila = new LinkedList<>();
+        if (inicio)
+        {
+            inicio = false;
+            fila.add(this);
+        }
+
+
+        resultado = fila.pollFirst();
+        
+        if (resultado != null)
+        {                
+            if (resultado.esquerda != null)
+                fila.add(resultado.esquerda);
+            if (resultado.direita != null)
+                fila.add(resultado.direita);
+        }
+
+
+        if (resultado == null)
+        {
+            tiraVisitado(this);
+            inicio = true;
+        }
+
+        return resultado;
+    } 
+
+
+
+//----------------------------------------------------------------------------------------------------------------------------------
+
     
     private void inicializaPilha() {
         if (pilha == null) {
@@ -96,9 +271,9 @@ public class ArvoreBinaria<E> {
     }
     
     /**
-     * Reinicia o percurso a partir do início.
-     * Deve ser chamado após percorrer toda a árvore para realizar novo
-     * percurso ou para voltar ao início a qualquer momento.
+     * Reinicia o percurso a partir do inÃ­cio.
+     * Deve ser chamado apÃ³s percorrer toda a Ã¡rvore para realizar novo
+     * percurso ou para voltar ao inÃ­cio a qualquer momento.
      */
     public void reinicia() {
         inicializaPilha();
@@ -108,8 +283,8 @@ public class ArvoreBinaria<E> {
     }
     
     /**
-     * Retorna o dado do próximo nó em-ordem.
-     * @return O dado do próximo nó em-ordem.
+     * Retorna o dado do prÃ³ximo nÃ³ em-ordem.
+     * @return O dado do prÃ³ximo nÃ³ em-ordem.
      */
     public ArvoreBinaria<E> proximoEmOrdem() {
         ArvoreBinaria<E> resultado = null;
@@ -126,36 +301,40 @@ public class ArvoreBinaria<E> {
             resultado = ultimoVisitado;
             ultimoVisitado = ultimoVisitado.direita;
         }
+        
+        if (resultado == null)
+            inicio = true;
+
         return resultado;
     }
     
     /**
-     * Retorna o dado armazenado no nó.
-     * @return O dado armazenado no nó.
+     * Retorna o dado armazenado no nÃ³.
+     * @return O dado armazenado no nÃ³.
      */
     public E getDado() {
         return dado;
     }
 
     /**
-     * Atribui um dado ao nó.
-     * @param dado O dado a ser atribuído ao nó.
+     * Atribui um dado ao nÃ³.
+     * @param dado O dado a ser atribuÃ­do ao nÃ³.
      */
     public void setDado(E dado) {
         this.dado = dado;
     }
 
     /**
-     * Retorna a árvore esqueda.
-     * @return A árvore esquerda.
+     * Retorna a Ã¡rvore esqueda.
+     * @return A Ã¡rvore esquerda.
      */
     protected ArvoreBinaria<E> getEsquerda() {
         return esquerda;
     }
 
     /**
-     * Retorna a árvore direita.
-     * @return A árvore direita.
+     * Retorna a Ã¡rvore direita.
+     * @return A Ã¡rvore direita.
      */
     protected ArvoreBinaria<E> getDireita() {
         return direita;
